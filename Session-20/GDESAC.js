325814d21608ad235ddf135524b190563b1d27ec
Etimen <aymenelidrysy@gmail.com>
2024-06-03 15:04:16 +0200

    commit

---
+++ Session-20/GDESAC.js
@@ -0,0 +1,16 @@
+document.getElementById("btn-hide").addEventListener("click",function(){
+    $("#title-1").css('color' ,'red')
+})
+
+
+
+
+$("#input-btn").focus()
+
+
+
+
+document.getElementById("btn-show").addEventListener("click",function(){
+
+    $("#title-3").show()
+})
